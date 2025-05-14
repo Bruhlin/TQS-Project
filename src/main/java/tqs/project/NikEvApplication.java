264a1a01c01d22f041d07311dcef97d20a8ef222
Bruhlin <joaomorais30478@gmail.com>
2025-05-14 18:21:28 +0100

    project created

---
+++ src/main/java/tqs/project/NikEvApplication.java
@@ -0,0 +1,13 @@
+package tqs.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikEvApplication.class, args);
+	}
+
+}
