af77dd50b8c22d944895ffd5d5dcdd003e19b740
Pedro Rei <pedrorrei@ua.pt>
2025-05-21 19:33:11 +0100

    api

--- src/main/java/tqs/project/NikEvApplication.java
+++ src/main/java/tqs/project/NikEvApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NikEvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NikEvApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NikEvApplication.class, args);
+    }
 }
